5dfae7c34326836ad074c9fcf859caae3fbd19df
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-23 23:57:55 +0100

    added Router Provider, createBrowserRouter & Outlet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+//layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={RootLayout}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobarouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main> 
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="about" element={<About/>}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  <RouterProvider router={router}/>
+  )
 }
 
 export default App;
