76c9a16242b1f31eafa208ed0f7346228fae6643
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-24 00:20:00 +0100

    corrected error in <Route element> so it outputs a component. Added margin-top and margin-bottom of 40px to <main>

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import RootLayout from "./layouts/RootLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={RootLayout}>
+    <Route path="/" element={<RootLayout/>}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
     </Route>
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
